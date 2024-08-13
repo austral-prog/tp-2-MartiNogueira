3a0e1cc9ad46fc9cd44618b8d8849f7ce71d482f
MartiNogueira <140777358+MartiNogueira@users.noreply.github.com>
2024-08-13 16:21:39 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,21 @@
 def change():
-    expense = 23.75
-    money = 100
+    expends = float(input("Ingresar gasto :"))
+    print(f"{expends}")
+
+
+
+    money = int(input("Dinero :"))
+    print(f"{money}")
+
+    print("\t")
+    print("Vuelto")
+    print("\t")
+
+    vuelto_int = int(money - expends)
+    print("Pesos :")
+    print(f"{vuelto_int}")
+
+
+    vuelto_float = int((((money - expends) - vuelto_int))*100)
+    print("Centavos :")
+    print(f"{vuelto_float}")
