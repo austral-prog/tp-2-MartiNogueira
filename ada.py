4d891ce364a513f6eb108e3ee0f47e7381892632
Martiniano <140777358+MartiNogueira@users.noreply.github.com>
2024-08-13 23:49:40 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,12 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
-
     complete_name = first_name + " " + last_name
-
     print(complete_name.lower())
     print(complete_name.title())
     print(complete_name.upper())
     print(f"\t{complete_name.lower()}")
-
 
