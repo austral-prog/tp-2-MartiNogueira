068c0728dcae515fcb51ee6a29beb08bb768cf7c
Martiniano <140777358+MartiNogueira@users.noreply.github.com>
2024-08-15 00:29:21 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,11 +1,21 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
-    full_name_1 = f"{first_name.lower()} {last_name.lower()}"
-    full_name_2 = f"{first_name.capitalize()} {last_name.capitalize()}"
-    full_name_3 = f"{first_name.upper()} {last_name.upper()}"
-    full_name_4 = f"\t{first_name.lower()} {last_name.lower()}"
-    print(full_name_1)
-    print(full_name_2)
-    print(full_name_3)
-    print(full_name_4)
+
+# Convertimos el nombre y el apellido a minúsculas
+    full_name_lowercase = f"{first_name.lower()} {last_name.lower()}"
+
+# Convertimos el nombre y el apellido a capitalizado (primera letra en mayúscula)
+    full_name_capitalized = f"{first_name.capitalize()} {last_name.capitalize()}"
+
+# Convertimos el nombre y el apellido a mayúsculas
+    full_name_uppercase = f"{first_name.upper()} {last_name.upper()}"
+
+# Output final con las cuatro formas
+    output = f"""{full_name_lowercase} 
+    {full_name_capitalized} 
+    {full_name_uppercase}  
+    \t{full_name_lowercase}"""
+
+    print(output)
+
