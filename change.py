2dedf7d9f68eb45c4972e4020d8f6e2bb1799ca4
Martiniano <140777358+MartiNogueira@users.noreply.github.com>
2024-08-15 00:18:54 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,24 +1,30 @@
 def change():
-    
+
     print("Ingresar gasto:")
     expends = 23.75
     print(f"{expends}")
     
+
     print("Dinero recibido")
     money = 100
     print(f"{money}")
     
+   
     print("")
     print("Vuelto")
     print("")
     
+   
     vuelto_completo = money - expends
-    pesos = int(vuelto_completo)  
+    pesos = int(vuelto_completo)  # Parte entera (pesos)
     centavos = int(round((vuelto_completo - pesos) * 100))  # Parte decimal (centavos)
-    
+   
     print("Pesos:")
     print(f"{pesos}")
     print("Centavos:")
     print(f"{centavos}")
+
+
+
 
 change()
